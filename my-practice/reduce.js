81c5dcbe652b9ace83319b70acc275a3d48d6db6
MBAsh0023 <92618879+MBAsh0023@users.noreply.github.com>
2021-11-09 13:33:28 -0800

    Delete my-practice directory

--- my-practice/reduce.js
+++
@@ -1,7 +0,0 @@
-const areas = [768, 1004.2, 433.1];
-
-let result = areas.reduce((acc, area, index) => {
-  console.log(`index: ${index}`, `acc: ${acc}`, `area: ${area}`);
-  return acc + area;
-});
-console.log(result); //> 2205.3
